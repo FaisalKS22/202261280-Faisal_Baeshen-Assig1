84e1697d41b60312e092140a64e97eb8d3087b91
FaisalKS22 <s202261280@gmail.com>
2026-05-21 02:17:02 +0300

    DEPLOY SOLVED

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/202261280-FaisalBaeshen-assignment4/' : '/',
+  base: command === 'build' ? '/202261280-Faisal_Baeshen-Assig1/' : '/',
 }))
